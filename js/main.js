1978591cb3073290c44bebb8fdf322587046a057
0blnak0 <li1061381003@126.com>
2022-08-20 14:19:44 +0800

    Site updated: 2022-08-20 14:19:44

--- js/main.js
+++ js/main.js
@@ -8,33 +8,3 @@ myImage.onclick = function() {
       myImage.setAttribute('src', 'css/images/banner.jpg');
     }
 }
-//添加个性化欢迎信息
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-setUserName();
-function setUserName() 
-{
-  let myName = prompt('请输入你的名字。');
-  if(!myName) 
-  {
-    setUserName();
-  } 
-  else 
-  {
-    localStorage.setItem('name', myName);
-    myHeading.textContent = '欢迎你,' + myName + '叼毛！';
-  }
-  if(!localStorage.getItem('name')) 
-	{
-	setUserName();
-	} 
-	else 
-	{
-	let storedName = localStorage.getItem('name');
-	myHeading.textContent = '欢迎你,' + myName + '叼毛！';
-	}
-}
-myButton.onclick = function() 
-{
-   setUserName();
-}
